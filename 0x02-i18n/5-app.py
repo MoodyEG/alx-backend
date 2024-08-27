754e3e6b071643de547161c5de6783965949ab46
MoodyEG <moodyeg2011@yahoo.com>
2024-08-27 21:38:12 +0300

    Testing 5

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 """ Back to flask """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config:
@@ -29,10 +37,24 @@ def get_locale() -> str:
 # babel.init_app(app, locale_selector=get_locale)
 
 
+def get_user() -> dict:
+    """ Get user from login_as parameter """
+    user_id = request.args.get('login_as')
+    if user_id is None:
+        return None
+    return users.get(int(user_id))
+
+
+@app.before_request
+def before_request():
+    """ Set user before each request """
+    g.user = get_user()
+
+
 @app.route("/", strict_slashes=False)
 def index() -> str:
     """ Index route """
-    return render_template("4-index.html")
+    return render_template("5-index.html")
 
 
 if __name__ == "__main__":
